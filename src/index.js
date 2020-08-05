39f18d7cfe8860329e1d5d27e2cd5351648b47b8
MichiMar <michimar.alexis@gmail.com>
2020-08-05 15:14:21 -0500

    user ath

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const tmi = require("tmi.js");
+
+const options = {
+  identity: {
+    username: "marekipa",
+    password: "oauth:txaelr19rjhwms2hbf0sszsdmlgyip",
+  },
+};
+
+tmi.client(options);
